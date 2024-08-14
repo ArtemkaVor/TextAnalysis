3a2d3d634c990eb8e2a309a1d62758fc400fb80f
Алёша Капибарович <barnkovich@mail.ru>
2024-08-14 23:25:43 +0700

    Set bigram and trigram function

--- FrequencyAnalysisTask.cs
+++ FrequencyAnalysisTask.cs
@@ -6,9 +6,44 @@ namespace TextAnalysis
 {
     static class FrequencyAnalysisTask
     {
+        private static Dictionary<string, Dictionary<string, int>> nGrams = new Dictionary<string, Dictionary<string, int>>();
         public static Dictionary<string, string> GetMostFrequentNextWords(List<List<string>> text)
         {
-            return new Dictionary<string, string> { };
+            foreach (var sentences in text)
+            {
+                for (int i = 0; i < sentences.Count - 1; i++)
+                {
+                    SetBigram(sentences, i);
+                    if (i < sentences.Count - 2) SetTrigram(sentences, i);
+                }
+            }
+            return new Dictionary<string, string> { }; // не забудь еблан
+        }
+
+        private static void SetBigram(List<string> sentence, int i)
+        {
+            var bigram = sentence[i];
+            var nextWord = sentence[i + 1];
+            if (!nGrams.ContainsKey(bigram))
+                nGrams[bigram] = new Dictionary<string, int>();
+
+            if (!nGrams[bigram].ContainsKey(nextWord))
+                nGrams[bigram][nextWord] = 0;
+
+            nGrams[bigram][nextWord]++;
+            
+        }
+        private static void SetTrigram(List<string> sentence, int i)
+        {
+            var trigram = sentence[i];
+            var nextNextWord = sentence[i + 2];
+            if (!nGrams.ContainsKey(trigram))
+                nGrams[trigram] = new Dictionary<string, int>();
+
+            if (!nGrams[trigram].ContainsKey(nextNextWord))
+                nGrams[trigram][nextNextWord] = 0;
+
+            nGrams[trigram][nextNextWord]++;
         }
     }
 }

154322c54f837dc5788a567afaf6032cd882c70f
ArtemkaVor <barnkovich@mail.ru>
2024-08-14 17:00:49 +0700

    Добавьте файлы проекта.

---
+++ FrequencyAnalysisTask.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace TextAnalysis
+{
+    static class FrequencyAnalysisTask
+    {
+        public static Dictionary<string, string> GetMostFrequentNextWords(List<List<string>> text)
+        {
+            return new Dictionary<string, string> { };
+        }
+    }
+}
---
+++ SentencesParserTask.cs
@@ -0,0 +1,42 @@
+﻿namespace TextAnalysis;
+
+static class SentencesParserTask
+{
+    public static List<List<string>> ParseSentences(string text)
+    {
+        char[] sentencesSeparators = new char[] { '.', '!', '?', ';', ':', '(', ')' };
+        var sentencesList = new List<List<string>>();
+        foreach (var sentence in text.Split(sentencesSeparators))
+        {
+            var words = SplitSentenceIntoWords(sentence);
+            if (words.Count > 0) sentencesList.Add(words);
+        }
+        return sentencesList;
+    }
+
+    public static List<string> SplitSentenceIntoWords(string sentence)
+    {
+        var words = new List<string>();
+        var word = "";
+
+        foreach (var ch in sentence)
+        {
+            if (char.IsLetter(ch) || ch == '\'')
+            {
+                word += ch;
+            }
+            else if (word.Length > 0)
+            {
+                words.Add(word.ToLower());
+                word = "";
+            }
+        }
+
+        if (word.Length > 0)
+        {
+            words.Add(word.ToLower());
+        }
+
+        return words;
+    }
+}

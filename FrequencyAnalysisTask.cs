c0c51cf1f0851b9e628a7a1946490f50e75b2342
ArtemkaVor <barnkovich@mail.ru>
2024-08-16 18:30:12 +0700

    activate test TextAnalysis.FrequencyAnalysis_Tests

--- FrequencyAnalysisTask.cs
+++ FrequencyAnalysisTask.cs
@@ -9,6 +9,7 @@ namespace TextAnalysis
         private static Dictionary<string, Dictionary<string, int>> nGrams = new Dictionary<string, Dictionary<string, int>>();
         public static Dictionary<string, string> GetMostFrequentNextWords(List<List<string>> text)
         {
+            nGrams.Clear();
             foreach (var sentences in text)
             {
                 for (int i = 0; i < sentences.Count - 1; i++)
@@ -17,7 +18,13 @@ namespace TextAnalysis
                     if (i < sentences.Count - 2) SetTrigram(sentences, i);
                 }
             }
-            return new Dictionary<string, string> { }; // не забудь еблан
+            var result = new Dictionary<string, string>();
+            foreach (var nGramSorted in nGrams)
+            {
+                var mostFrequentNextWords = nGramSorted.Value.OrderByDescending(x => x.Value).ThenBy(x=> x.Key, StringComparer.Ordinal).First().Key;
+                result[nGramSorted.Key] = mostFrequentNextWords;
+            }
+            return result;
         }
 
         private static void SetBigram(List<string> sentence, int i)
@@ -35,8 +42,9 @@ namespace TextAnalysis
         }
         private static void SetTrigram(List<string> sentence, int i)
         {
-            var trigram = sentence[i];
+            var trigram = sentence[i] + " " + sentence[i + 1];
             var nextNextWord = sentence[i + 2];
+
             if (!nGrams.ContainsKey(trigram))
                 nGrams[trigram] = new Dictionary<string, int>();
 
